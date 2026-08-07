f306b1c734a14544f72c65aa0d10da3787779b42
Ajay Singh <ajaysingh@Ajays-MacBook-Air.local>
2026-08-07 17:39:59 +0530

    json server

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Product} from './services/product'
+import { User } from './interfaces/User';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-api');
 
-  productList:any
+  productList:User[]=[];
   constructor(private pService: Product){
 
   }
   ngOnInit(){
-    this.pService.getProductList().subscribe((data:any)=>{
-      //console.log(data);
-      this.productList=data.products;
+    this.pService.getUser().subscribe((data:User[])=>{
+      console.log('Received', data.length);
+      console.log(data);
+      this.productList=data;
     })
   }
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Service ,inject} from '@angular/core';
+import { inject,Service} from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/User';
 
 @Service()
 export class Product {
 
    private http = inject(HttpClient);
 
-    getProductList(){
-        const url="https://dummyjson.com/products";
-        return this.http.get(url);
+    getUser():Observable<User[]>{
+        const url="http://localhost:3000/users";
+        return this.http.get<User[]>(url);
     }
 }
